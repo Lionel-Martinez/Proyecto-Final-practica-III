5117c27d02bc5a71370231ab3f80dae1e28554ed
Lionel Martinez <liojosemartinez@gmail.com>
2026-09-17 18:07:50 -0300

    Agregar usuarios de prueba para roles tecnico y mostrador

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,17 +13,37 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
+{
+    // User::factory(10)->create();
 
-        User::updateOrCreate(
-            ['username' => 'admin_ulicel'],
-            [
-                'name' => 'Administrador Ulicel',
-                'email' => 'admin@ulicel.local',
-                'role' => 'admin',
-                'password' => Hash::make('Cambiar123!'),
-            ],
-        );
-    }
+    User::updateOrCreate(
+        ['username' => 'admin_ulicel'],
+        [
+            'name' => 'Administrador Ulicel',
+            'email' => 'admin@ulicel.local',
+            'role' => 'admin',
+            'password' => Hash::make('Cambiar123!'),
+        ],
+    );
+
+    User::updateOrCreate(
+        ['username' => 'tecnico_ulicel'],
+        [
+            'name' => 'Técnico Ulicel',
+            'email' => 'tecnico@ulicel.local',
+            'role' => 'tecnico',
+            'password' => Hash::make('Cambiar123!'),
+        ],
+    );
+
+    User::updateOrCreate(
+        ['username' => 'mostrador_ulicel'],
+        [
+            'name' => 'Mostrador Ulicel',
+            'email' => 'mostrador@ulicel.local',
+            'role' => 'mostrador',
+            'password' => Hash::make('Cambiar123!'),
+        ],
+    );
+}
 }
